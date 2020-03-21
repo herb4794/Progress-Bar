1acb19f093d46a4764ddbffcfbebb510ddaaa655
Lawrence <herb4794@gmail.com>
2020-03-22 00:04:08 +0800

    Ruby 功課

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+#Rails.application.configure do
+#    config.ganerators do[g]
+#        g.javascript_engine :js
+#    end
+#end 

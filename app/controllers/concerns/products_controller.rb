3a346b0af2592b01acbec0a0e361415cf97da6e4
Lawrence <herb4794@gmail.com>
2020-04-25 22:57:05 +0800

    上傳圖檔與檔案操作

--- app/controllers/concerns/products_controller.rb
+++ app/controllers/concerns/products_controller.rb
@@ -42,8 +42,14 @@ class ProductsController < ApplicationController
         @product = Product.new
     end
 
-    def create
-        product = Product.create(product_permit)
+    def create 
+        image = params[:product][:image]
+        if image
+        image_url = save_file(image)
+        end
+
+        product = Product.create(product_permit.merge({image_url: image_url}))
+
         flash[:notice] = "建立成功"
         redirect_to action: :new
 
@@ -59,7 +65,14 @@ class ProductsController < ApplicationController
 
     def update
         product = Product.find(params[:id])
-        product.update(product_permit)
+
+        image = params[:product][:image]
+        if image
+            image_url = save_file(image)
+            product.update(product_permit.merge({image_url: image_url}))
+        else
+            product.update(product_permit)
+        end
 
         flash[:notice] = "更新成功"
         redirect_to action: :edit
@@ -78,7 +91,7 @@ class ProductsController < ApplicationController
     # end
 
     def product_permit
-        params.require(:product).permit([:name, :description, :image_url, :price, :subcategory_id])
+        params.require(:product).permit([:name, :description, :price, :subcategory_id])
     end
 
     def redirect_to_root_if_not_log_in
@@ -89,5 +102,17 @@ class ProductsController < ApplicationController
         end
     end
 
-    
+    def save_file(newFile)
+        dir_url = Rails.root.join('public', 'uploads/products')
+
+        FileUtils.mkdir_p(dir_url) unless File.directory?(dir_url)    
+
+        file_url = dir_url + newFile.original_filename
+
+        File.open(file_url, 'w+b') do |file|
+            file.write(newFile.read)
+        end
+
+        return "/uploads/products/" + newFile.original_filename
+    end
 end

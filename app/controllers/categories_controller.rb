591e4210ddcabe17e9197d02fd24061e33964d70
Lawrence <herb4794@gmail.com>
2020-05-01 22:39:57 +0800

    完成分類(Category)與副分類(Subcategory) - 透過has_may_through

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,39 @@
 class CategoriesController < ApplicationController
     before_action :get_category, only: [:products]
 
+    LIMITED_PRODUCTS_NUMBER = 20
+
     def products
+        @ad = {
+            title: "大型廣告",
+            des: "這是廣告!!!",
+            action_title: "閱讀更多",
+        }
+
+        if params[:page]
+            @page = params[:page].to_i
+        else
+            @page = 1
+        end
+
+        @categories = Category.all
+
+        # TODO: tmp
+        @products = @category.products
+
+        @first_page = 1
+        count = @products.count
+
+        @last_page = (count / LIMITED_PRODUCTS_NUMBER)
+        if count % LIMITED_PRODUCTS_NUMBER
+        @last_page  += 1
+        end
+        
+        @products = @products.offset((@page - 1) *
+        LIMITED_PRODUCTS_NUMBER).limit(LIMITED_PRODUCTS_NUMBER)
     end
 
     def get_category
+        @category = Category.find_by_id(params[:category_id])
     end
 end
--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -2,12 +2,45 @@ class SubcategoriesController < ApplicationController
     before_action :get_category, only: [:products]
     before_action :get_subcategory, only: [:products]
 
+    LIMITED_PRODUCTS_NUMBER = 20
+
     def products
+        @ad = {
+            title: "大型廣告",
+            des: "這是廣告!!!",
+            action_title: "閱讀更多",
+        }
+
+        if params[:page]
+            @page = params[:page].to_i
+        else
+            @page = 1
+        end
+
+        @categories = Category.all
+
+        @products = @subcategory.products
+
+        @first_page = 1
+        count = @products.count
+
+        @last_page = (count / LIMITED_PRODUCTS_NUMBER)
+        if count % LIMITED_PRODUCTS_NUMBER
+        @last_page  += 1
+        end
+
+        @products = @products.offset((@page - 1) *
+        LIMITED_PRODUCTS_NUMBER).limit(LIMITED_PRODUCTS_NUMBER)
     end
 
     def get_category
+        @category = Category.find_by(params[:category_id])
     end
 
     def get_subcategory
+        @subcategory = Subcategory.find_by(params[:subcategory_id])
+        if(@subcategory.category != @category)
+            redirect_to products_category_path(@category)
+        end
     end
 end
